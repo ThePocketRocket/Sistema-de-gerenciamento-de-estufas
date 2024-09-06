de3b9e1a85acfec3550e4f77fa6fef81c5eb09fe
Leonardo <leonardolv1112@gmail.com>
2024-09-06 20:07:45 -0300

    commit para testar o merge

--- src/system/ISensores.java
+++ src/system/ISensores.java
@@ -8,4 +8,21 @@ public class ISensores {
 		this.id = id;
 		this.numSerie = numSerie;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getNumSerie() {
+		return numSerie;
+	}
+
+	public void setNumSerie(int numSerie) {
+		this.numSerie = numSerie;
+	};
+	
 }
