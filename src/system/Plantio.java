9e2540f5fba1764a2a366f69d1f8e76bbd3b1b1e
Daniel Ferreira <pocketrocket@outlook.com.br>
2024-09-07 21:56:42 -0300

    Update Plantio.java

--- src/system/Plantio.java
+++ src/system/Plantio.java
@@ -1,4 +1,57 @@
 package system;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Plantio {
+    private static final AtomicInteger contador = new AtomicInteger(0);
+    private int idPlantio;
+    private ArrayList <String> substancias = new ArrayList<>();
+    private String nomeEspecie;
+    private String inicio, fim;
+
+    public Plantio (String nomeEspecie) {
+        this.idPlantio = contador.incrementAndGet();
+        this.nomeEspecie = nomeEspecie;
+        this.inicio = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+    }
+
+    public void setNomeEspecie(String nomeEspecie) {
+        this.nomeEspecie = nomeEspecie;
+    }
+
+    public void setInicio() {
+        this.inicio = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+    }
+
+    public void setFim() {
+        this.fim = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+    }
+
+    public void setSubstancias(String substancia) {
+        substancias.add(substancia);
+    }
+
+    public int getIdPlantio() {
+        return idPlantio;
+    }
+
+    public ArrayList<String> getSubstancias() {
+        return (ArrayList<String>) Collections.unmodifiableList(new ArrayList<>(this.substancias));
+    }
+
+    public String getNomeEspecie() {
+        return nomeEspecie;
+    }
+
+    public String getInicio() {
+        return inicio;
+    }
+
+    public String getFim() {
+        return fim;
+    }
 }
