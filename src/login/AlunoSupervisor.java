8e693228401120efdd75afe9f56c7c7f4c242197
Leonardo <leonardolv1112@gmail.com>
2024-09-10 16:10:36 -0300

    Inicio da criação de metodos para o Menu

--- src/login/AlunoSupervisor.java
+++ src/login/AlunoSupervisor.java
@@ -1,12 +1,20 @@
 package login;
-
+import system.Plantio;
 public class AlunoSupervisor extends User {
+	
+
+
     public AlunoSupervisor(String nome, String senha, int nivel) {
 
         super(nome, senha, nivel);
     }
 
-    public String gerarRelatorioPlantio() {
+    public String gerarRelatorioPlantio(Plantio p1) {
+    	   System.out.println("O ID do Plantio é: " + p1.getIdPlantio() + "/n" + "O plantio é: " +
+                   p1.getNomeEspecie() + "/n" + "As substâncias são: " + p1.getSubstancias() + "/n"
+                   + "Inicio do plantio:" + p1.getInicio() + "/n" + " Data para colheita: " + p1.getFim());
+
+    	
         // irei adicionar a geração do relatórioS
         return "Relatório de Plantio Gerado.";
     }
@@ -15,6 +23,7 @@ public class AlunoSupervisor extends User {
 
         return "Relatório de Aluno Gerado.";
     }
+
 }
 
 
