525ca52157a7ce030563d07e7b28a0d406d1ffce
Leonardo <leonardolv1112@gmail.com>
2024-09-10 23:41:15 -0300

    Finalização das exceções

--- src/login/AlunoSupervisor.java
+++ src/login/AlunoSupervisor.java
@@ -1,35 +1,54 @@
 package login;
+
 import main.Menu;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import exceptions.inputVazioException;
+
 public class AlunoSupervisor extends User {
-    public AlunoSupervisor(String nome, String senha, int nivel) {
-        super(nome, senha, nivel);
-    }
+	public AlunoSupervisor(String nome, String senha, int nivel) {
+		super(nome, senha, nivel);
+	}
 
-    public static void gerarRelatorioAluno(Scanner sc) {
-        System.out.print("Digite o título do relatório: ");
-        String titulo = sc.nextLine();
-        System.out.print("Digite a descrição do relatório: ");
-        String descricao = sc.nextLine();
+	public static void gerarRelatorioAluno(Scanner sc) {
 
-        System.out.print("Digite o ID do Aluno: ");
-        int idAluno = sc.nextInt();
+		System.out.print("Digite o título do relatório: ");
+		
+			String titulo = sc.nextLine();
+			System.out.print("Digite a descrição do relatório: ");
+			String descricao = sc.nextLine();
 
-        String nome = "Não encontrado";
-        for (User aluno : Menu.users) {
-            if (aluno.getId() == idAluno) {
-                nome = aluno.getNome();
-                break;
-            }
-        }
-        System.out.printf("""
-                Aluno: %s
-                Título: %s
-                Descrição: %s
-                """, nome, titulo, descricao);
-    }
+			String nome = "Não encontrado";
+			
+			
+			boolean idfind = false;
+			while(!idfind) {
+				try {
+			System.out.print("Digite o ID do Aluno: ");
+			int idAluno = sc.nextInt();
+				sc.nextLine();
+				
+			
+			for (User aluno : Menu.users) {
+				if (aluno.getId() == idAluno) {
+					nome = aluno.getNome();
+					idfind = true;
+					break;
+				}
+			}
+			}catch(InputMismatchException e) {
+				System.out.println("Erro: id invalido, coloque um id valido");
+				sc.nextLine();
+			}
+				}
+		
+			System.out.printf("""
+					Aluno: %s
+					Título: %s
+					Descrição: %s
+					""", nome, titulo, descricao);
+				
+		}
 }
-
-
