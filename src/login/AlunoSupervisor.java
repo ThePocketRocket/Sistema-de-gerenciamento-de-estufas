240bef9a448d68937b39cfdf281ab89678ef5195
Daniel Ferreira <pocketrocket@outlook.com.br>
2024-09-10 22:52:58 -0300

    Update AlunoSupervisor.java

--- src/login/AlunoSupervisor.java
+++ src/login/AlunoSupervisor.java
@@ -1,22 +1,11 @@
 package login;
 import main.Menu;
-import system.Plantio;
 
 import java.util.Scanner;
 
 public class AlunoSupervisor extends User {
     public AlunoSupervisor(String nome, String senha, int nivel) {
-
         super(nome, senha, nivel);
-    }
-
-    public String gerarRelatorioPlantio(Plantio p1) {
-    	   System.out.println("O ID do Plantio é: " + p1.getIdPlantio() + "/n" + "O plantio é: " +
-                   p1.getNomeEspecie() + "/n" + "As substâncias são: " + p1.getSubstancias() + "/n"
-                   + "Inicio do plantio:" + p1.getInicio() + "/n" + " Data para colheita: " + p1.getFim());
-           
-        // irei adicionar a geração do relatórioS
-        return "Relatório de Plantio Gerado.";
     }
 
     public static void gerarRelatorioAluno(Scanner sc) {
